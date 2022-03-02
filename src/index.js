0b8ec25bc2ef1b6a9b65345077031e54e4a480ea
n0moreCRY <EugenGavrisciuc@yandex.ru>
2022-03-02 13:48:20 +0200

    REDUX Training

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import {createStore} from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1
+    case "DEC":
+      return state - 1
+    case "RND":
+      return action.tempval;
+    case "CLR":
+      return state = 0
+    default:
+      return state
+  }
+}
+
+const state = createStore(reducer),
+      inc = () => ({type: "INC"}),
+      dec = () => ({type: "DEC"}),
+      rnd = (tempval) => ({type: "RND", tempval}),
+      clr = () => ({type: "CLR"});
+
+document.getElementById("INC").addEventListener("click", () => {
+  state.dispatch(inc());
+})
+
+document.getElementById("DEC").addEventListener("click", () => {
+  state.dispatch(dec())
+})
+
+document.getElementById("RND").addEventListener("click", () => {
+  const tempval = Math.round(Math.random() * 100);
+  state.dispatch(rnd(tempval))
+})
+
+document.getElementById("CLR").addEventListener("click", () => {
+  state.dispatch(clr());
+})
+
+const update = () => {
+  document.getElementById("val").textContent = state.getState();
+}
+
+state.subscribe(update);
